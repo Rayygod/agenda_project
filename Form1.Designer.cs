65b67baac2ec6127d8550554ab5c7a3fea607f28
Vito <vitorfonsecaxs@gmail.com>
2024-10-08 10:50:54 -0300

    Tela de Login feita

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace agenda_project
 {
-    partial class Form1
+    partial class FrmLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            btn_entrar = new Button();
+            btn_cadastrar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(349, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 47);
+            label1.TabIndex = 0;
+            label1.Text = "User";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(284, 79);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(236, 66);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(282, 217);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(236, 66);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(340, 167);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 47);
+            label2.TabIndex = 2;
+            label2.Text = "Senha";
+            // 
+            // btn_entrar
+            // 
+            btn_entrar.BackColor = Color.Blue;
+            btn_entrar.Cursor = Cursors.Hand;
+            btn_entrar.Enabled = false;
+            btn_entrar.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_entrar.ForeColor = Color.White;
+            btn_entrar.Location = new Point(239, 334);
+            btn_entrar.Name = "btn_entrar";
+            btn_entrar.Size = new Size(138, 52);
+            btn_entrar.TabIndex = 4;
+            btn_entrar.Text = "Entrar";
+            btn_entrar.UseVisualStyleBackColor = false;
+            // 
+            // btn_cadastrar
+            // 
+            btn_cadastrar.BackColor = Color.Red;
+            btn_cadastrar.Cursor = Cursors.Hand;
+            btn_cadastrar.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_cadastrar.ForeColor = Color.White;
+            btn_cadastrar.Location = new Point(416, 334);
+            btn_cadastrar.Name = "btn_cadastrar";
+            btn_cadastrar.Size = new Size(138, 52);
+            btn_cadastrar.TabIndex = 5;
+            btn_cadastrar.Text = "Cadastrar";
+            btn_cadastrar.UseVisualStyleBackColor = false;
+            btn_cadastrar.Click += btn_cadastrar_Click;
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_cadastrar);
+            Controls.Add(btn_entrar);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "FrmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FrmLogin";
+            FormClosing += FrmLogin_FormClosing;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button btn_entrar;
+        private Button btn_cadastrar;
     }
 }
